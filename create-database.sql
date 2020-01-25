484a12ea5cc95402256b72b7aa39421dbd2a7009
TuRaMbARr <zangeneh.saeed411@yahoo.com>
2020-01-25 20:07:46 +0330

    Add some tables

--- create-database.sql
+++ create-database.sql
@@ -1,3 +1,6 @@
+use master;
+DROP DATABASE BloodTransfusion;
+
 IF NOT EXISTS 
    (
      SELECT name FROM master.dbo.sysdatabases 
@@ -8,18 +11,15 @@ GO
 
 use BloodTransfusion;
 
-CREATE TABLE tab_events ( id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
-                          event_name STRING NOT NULL,
-                          event_day ENUM ('Mon','Tue','Wed','Thu','Fri','Sat','Sun'),
-                          status STRING CHECK (status in ('Active','Inactive') )
-                         );
+-- DROP TABLE BloodTransporter;
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='BloodTransporter' and xtype='U')
-    CREATE TABLE BloodTransporter (
-        id INTEGER PRIMARY KEY,
-        firstName NVARCHAR(64) COLLATE PERSIAN_100_CI_AI NOT NULL,
-        lastName NVARCHAR(64) COLLATE PERSIAN_100_CI_AI NOT NULL,
-    )
+CREATE TABLE BloodTransporter (
+    id INTEGER PRIMARY KEY,
+    firstName NVARCHAR(64) COLLATE PERSIAN_100_CI_AI NOT NULL,
+    lastName NVARCHAR(64) COLLATE PERSIAN_100_CI_AI NOT NULL,
+    bloodType NVARCHAR(10) NOT NULL CHECK (bloodType IN(N'O-', N'O+', N'A+', N'A-', N'B+', N'B-', N'AB+', N'AB-'))
+)
 GO
 
 IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Nurse' and xtype='U')
@@ -27,5 +27,45 @@ IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Nurse' and xtype='U')
         employeeId INTEGER PRIMARY KEY,
         firstName NVARCHAR(64) COLLATE PERSIAN_100_CI_AI NOT NULL,
         lastName NVARCHAR(64) COLLATE PERSIAN_100_CI_AI NOT NULL,
+        hiringDate SMALLDATETIME NOT NULL,        
+    )
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='WorksAt' and xtype='U')
+    CREATE TABLE BloodBank (
+        id INTEGER PRIMARY KEY,
+        bankAddress NVARCHAR(200) COLLATE PERSIAN_100_CI_AI NOT NULL,
+        bankName NVARCHAR(64) COLLATE PERSIAN_100_CI_AI NOT NULL,
+    )
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='WorksAt' and xtype='U')
+    CREATE TABLE WorksAt (
+        employeeId INTEGER,
+        bloodBankId INTEGER,
+        startTime SMALLDATETIME NOT NULL,
+        endTime SMALLDATETIME, 
+        PRIMARY KEY (employeeId, bloodBankId),
+        FOREIGN KEY (employeeId) REFERENCES Nurse(employeeId),
+        FOREIGN KEY (bloodBankId) REFERENCES BloodBank(id),
+    )
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='HealthReport' and xtype='U')
+    CREATE TABLE HealthReport (
+        id INTEGER NOT NULL,
+        pressure INTEGER NOT NULL CHECK (pressure <= 20 AND pressure >= 3),
+        temperature INTEGER NOT NULL CHECK (temperature < 50 AND temperature > 30),
+        density INTEGER NOT NULL,
+        testTime DATETIME NOT NULL,
+        happendAt INTEGER NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (happendAt) REFERENCES BloodBank(id),
+    )
+GO
+
+IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Donation' and xtype='U')
+    CREATE TABLE Donation (
+        id INTEGER NOT NULL,
     )
 GO
